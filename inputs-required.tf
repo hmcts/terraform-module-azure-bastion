1f1e7dcea33724a5b5d16abac10167cc50312a0d
Chirag Pareek <chirag.pareek@justice.gov.uk>
2023-02-02 16:55:36 +0000

    updated after first commit

--- inputs-required.tf
+++ inputs-required.tf
@@ -2,10 +2,9 @@ variable "env" {
   description = "Environment value."
   type        = string
 }
-
-variable "common_tags" {
-  description = "Common tag to be applied to resources."
-  type        = map(string)
+variable "builtFrom" {
+  description = "Built from value."
+  type        = string
 }
 
 variable "product" {
@@ -20,4 +19,55 @@ variable "project" {
 variable "component" {
   description = "https://hmcts.github.io/glossary/#component"
   type        = string
+}
+
+
+variable "resource_group_name" {
+  description = "Resource group name that contains the azure bastion host"
+  type        = string
+}
+
+
+variable "azbastion_subnet_address" {
+  description = "Virtual network subnet address that contains the azure bastion host"
+
+}
+
+variable "virtual_network_name" {
+  description = "Virtual network name that contains the bastion host"
+  type        = string
+}
+
+variable "public_ip_name" {
+  type        = string
+  description = "Public IP of the Bastion"
+}
+variable "allocation_method" {
+  type        = string
+  description = "Specify Static or Dynamic"
+}
+
+variable "public_ip_sku" {
+  type        = string
+  description = "Enter the SKU for the Public IP of the Bastion"
+}
+
+variable "bastion_name" {
+  type        = string
+  description = "Name of the bastion host"
+}
+
+variable "bastion_sku" {
+  type        = string
+  description = "Specify SKU for the bastion host"
+}
+
+variable "tunneling_enabled" {
+  type        = bool
+  description = "Select True or False, tunneling is only supported when sku is Standard"
+}
+
+variable "ip_config_name" {
+  type        = string
+  description = "IP config name"
 }
